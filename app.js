c4792630b1c5024d32505ac496c81e4e51d89647
Guillem Gelabert Sunyer <guillem@MacBookeGuillem.lan>
2019-02-21 14:13:54 +0100

    refactor to improve testability

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const { json, urlencoded } = require('body-parser');
+
+const app = express();
+
+require('./db');
+
+const user = require('./controllers/user');
+const card = require('./controllers/card');
+
+app
+  .use(cors())
+  .use(json())
+  .use(logger('dev'))
+  .use(urlencoded({ extended: true }))
+  .get('/users', user.getAll)
+  .get('/cards/all/:id', card.getAllCards) // gets all scheduled cards for user id
+  .get('/cards/due/:id', card.getDueCards) // gets all scheduled cards and adds new ones
+  .put('/cards', card.updateCard); // puts updateed card (correct or not) through sm2
+
+module.exports = app;
